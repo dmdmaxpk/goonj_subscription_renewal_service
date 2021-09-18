3cf49972a59b5d502b49db09435691361bdb49ac
Farhan Ali <farhan.ali@dmdmax.com>
2021-09-18 18:42:32 +0500

    billing cycles updated

--- config.js
+++ config.js
@@ -24,8 +24,7 @@ const codes = {
     code_otp_not_found: 17
 }
 
-//const tp_billing_cycle_hours = [1,5,8,11,14,17,20,22];
-const tp_billing_cycle_hours = [1,5,8];
+const tp_billing_cycle_hours = [1,5,8,11,14,17,20,22];
 const ep_billing_cycle_hours = [4,10,16];
 
 
